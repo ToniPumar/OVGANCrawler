9680fb253cf94d374c41ca87e77163bb4a657f82
ToniPu <tonidebux@gmail.com>
2024-03-28 17:41:00 +0100

    Inicio

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('adfadfadsfadsfafd')
+import puppeteer, { Browser, Page } from 'puppeteer'
+
+/* Esta funcion se está preparando para despues poderse eejcutar en firebase
+   no dividiremos el código en varias funciónes si no que intentaremo hacerlo todo con una sola
+   para garantizar que cada instancia de navegador se ejecuta por separado
+*/
+
+enum Accion
+{
+  LOGIN = 0,
+  ULTIMOSDIVS = 1,
+  DECLAMOVEMENTO = 2,
+  ALTANACEMENTO = 3
+}
+
+async function ovgan (user: string = '', password: string = '', act: Accion = Accion.LOGIN): Promise<string>
+{
+  const navegador: Browser = await puppeteer.launch();
+  const pagina: Page = await navegador.newPage();
+  await pagina.goto("https://ovgan.xunta.gal/ovgan/pin/login ");
+
+  return 'adfadf';
+}
