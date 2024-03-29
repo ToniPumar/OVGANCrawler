16049323804860b15e82b302cd42367542f1daeb
ToniPu <tonidebux@gmail.com>
2024-03-29 17:54:14 +0100

    INICIO DE VERFICIACION DE LOGIN

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Browser, Page } from 'puppeteer'
+import puppeteer, { type Browser, type HTTPResponse, type Page } from 'puppeteer';
 
 /* Esta funcion se está preparando para despues poderse eejcutar en firebase
    no dividiremos el código en varias funciónes si no que intentaremo hacerlo todo con una sola
@@ -7,17 +7,53 @@ import puppeteer, { Browser, Page } from 'puppeteer'
 
 enum Accion
 {
-  LOGIN = 0,
-  ULTIMOSDIVS = 1,
-  DECLAMOVEMENTO = 2,
-  ALTANACEMENTO = 3
+  LOGIN,
+  ULTIMOSDIVS,
+  DECLAMOVEMENTO,
+  ALTANACEMENTO
 }
 
-async function ovgan (user: string = '', password: string = '', act: Accion = Accion.LOGIN): Promise<string>
-{
-  const navegador: Browser = await puppeteer.launch();
+enum Error {
+  NOERROR,
+  URLLOGIN, // ERRO NA URL DO LOGIN
+  URLPRINCIPAL, // ERRO NA URL PRINCIPAL
+  ERRORELEMNTOSLOGIN, // ERROR CAPTURANDO OS ELMENTOS DO LOGIN
+  LOGIN // ERRO O FACER LONGIN
+}
+
+enum URLS {
+  LOGIN = 'https://ovgan.xunta.gal/ovgan/pin/login',
+  ALTAPORNACEMENTO = 'https://ovgan.xunta.gal/ovgan/gl/index.html#/misAltasNacimiento/solicitud',
+  DECLARACIONDEMOVEMENTO = 'https://ovgan.xunta.gal/ovgan/gl/index.html#/misDeclaracionMovimientosEntradas/solicitud',
+  ULTIMOSDIVS = 'https://ovgan.xunta.gal/ovgan/gl/index.html#/dibs'
+}
+
+async function ovgan (user: string = '', password: string = '', act: Accion = Accion.LOGIN): Promise<[Error, string]> {
+  const navegador: Browser = await puppeteer.launch({ headless: false });
   const pagina: Page = await navegador.newPage();
-  await pagina.goto("https://ovgan.xunta.gal/ovgan/pin/login ");
+  const response: HTTPResponse | null = await pagina.waitForResponse(URLS.LOGIN);
 
-  return 'adfadf';
+  if (response == null || response?.status() !== 200) {
+    return [Error.URLLOGIN, ''];
+  }
+  const username = await pagina.$('[name="username"]');
+  const pass = await pagina.$('[name="password"]');
+  const btnEnviar = await pagina.$('#botonentrarlogin');
+  if (username == null || pass == null || btnEnviar == null) {
+    return [Error.ERRORELEMNTOSLOGIN, ''];
+  }
+
+  await username.click();
+  await username.type(user);
+  await pass.click();
+  await pass.type(password);
+  await btnEnviar.click();
+  // COMPROBAR SE SE FIXON BEN O LOGIN
+  if (act === Accion.LOGIN) {
+    return [Error.NOERROR, 'Login correcto'];
+  }
+
+  return [Error.NOERROR, 'd'];
 }
+
+void ovgan('76347101G', 'SRKWYK', Accion.LOGIN);
