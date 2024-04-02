bf26b3201af0fec025ef82ef87c245db2478e795
ToniPu <tonidebux@gmail.com>
2024-04-02 12:00:07 +0200

    Finalizacion de login

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import puppeteer, { type Browser, type HTTPResponse, type Page } from 'puppeteer';
+import puppeteer, { type ElementHandle, type Browser, type HTTPResponse, type Page } from 'puppeteer';
 
 /* Esta funcion se está preparando para despues poderse eejcutar en firebase
    no dividiremos el código en varias funciónes si no que intentaremo hacerlo todo con una sola
    para garantizar que cada instancia de navegador se ejecuta por separado
 */
 
-enum Accion
-{
+enum Accion {
   LOGIN,
   ULTIMOSDIVS,
+  ULTIMOSFICHEIROS,
   DECLAMOVEMENTO,
   ALTANACEMENTO
 }
@@ -33,12 +33,15 @@ async function ovgan (user: string = '', password: string = '', act: Accion = Ac
   const pagina: Page = await navegador.newPage();
   const response: HTTPResponse | null = await pagina.waitForResponse(URLS.LOGIN);
 
-  if (response == null || response?.status() !== 200) {
+  if (response == null || response.status() !== 200) {
     return [Error.URLLOGIN, ''];
   }
-  const username = await pagina.$('[name="username"]');
-  const pass = await pagina.$('[name="password"]');
-  const btnEnviar = await pagina.$('#botonentrarlogin');
+
+  // Capturamos botones de login y enviamos peticion
+  const username: ElementHandle<Element> | null = await pagina.$('[name="username"]');
+  const pass: ElementHandle<Element> | null = await pagina.$('[name="password"]');
+  const btnEnviar: ElementHandle<Element> | null = await pagina.$('#botonentrarlogin');
+
   if (username == null || pass == null || btnEnviar == null) {
     return [Error.ERRORELEMNTOSLOGIN, ''];
   }
@@ -48,9 +51,25 @@ async function ovgan (user: string = '', password: string = '', act: Accion = Ac
   await pass.click();
   await pass.type(password);
   await btnEnviar.click();
-  // COMPROBAR SE SE FIXON BEN O LOGIN
+
+  // Esperamos 5 segundos a que resolva a peticion
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
+  const usuarioElement = await pagina.$('#header-user-name span');
+  const txtusuarioElement: string | null | undefined = await usuarioElement?.evaluate(element => element.textContent);
+
+  if (txtusuarioElement === 'Anónimo' || txtusuarioElement === undefined || txtusuarioElement === null) {
+    return [Error.LOGIN, 'Login incorrecto'];
+  }
+
   if (act === Accion.LOGIN) {
     return [Error.NOERROR, 'Login correcto'];
+  }
+
+  // Empezamos con las gestiones de datos
+
+  if (act === Accion.ULTIMOSDIVS) {
+    return [Error.NOERROR, ''];
   }
 
   return [Error.NOERROR, 'd'];
